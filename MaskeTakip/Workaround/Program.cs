60f64a38d0339f3d6563cf3fac7a2b6b2ab2c4b0
H. Erdem Anbarkaya <anbarkaya46@gmail.com>
2024-02-18 22:20:21 +0300

    İlk gün ikinci ödev ilk commit.

---
+++ MaskeTakip/Workaround/Program.cs
@@ -0,0 +1,36 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        Degiskenler();
+        Vatandas vatandas1 = new Vatandas();
+        
+        
+        static void Degiskenler()
+        {
+            string mesaj = "Merhaba";
+            double tutar = 10000; //Databaseden gelir.
+            int sayi = 100;
+            bool girisYapmisMi = false;
+
+            string ad = "Engin";
+            string soyad = "Demiroğ";
+            int dogumYili = 1985;
+            long tcNo = 12345678910;
+
+            Console.WriteLine(tutar * 1.18);
+            Console.WriteLine(tutar * 1.18);
+        }
+    }
+}
+//Pascal Case, public olduğunda fieldların da ilk harfi büyük olur. 
+public class Vatandas
+{
+    public string Ad { get; set; }
+    public string Soyad { get; set; }
+    public int DogumYili { get; set; }
+    public long TcNo { get; set; }
+}
